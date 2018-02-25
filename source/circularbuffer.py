234a069a935363fe1d48b3a2b22b98b0bbb36a92
ajboxjr <ajboxjr@gmail.com>
2018-02-25 00:56:57 -0500

    add circular buffer

--- source/circularbuffer.py
+++ source/circularbuffer.py
@@ -4,11 +4,10 @@ class CircularBuffer(object):
     def __init__(self,max_size=4):
         self.ll = LinkedList()
         self.max_size = max_size
-        self.counter = 0
         self.start_pointer = 0
         self.end_pointer = 0
-        for _ in range(max_size):
-            self.ll.append(None)
+        # for _ in range(max_size):
+        #     self.ll.append(None)
 
     def __repr__(self):
         return self.ll.__str__()
@@ -22,62 +21,63 @@ class CircularBuffer(object):
             if node.data is not None:
                 count +=1
         return count
+
     def get_at_index(self,index):
         return self.ll.get_at_index(index)
 
     def is_empty(self):
-        for node in self.ll:
-            if node.data is not None:
-                return False
-        return True
+        return  self.ll.size < self.max_size
+        # for node in self.ll:
+        #     if node.data is not None:
+        #         return False
+        # return True
 
     def is_full(self):
-        for node in self.ll:
-            if node.data is None:
-                return False
-        return True
+        return self.ll.size == self.max_size
 
     def move_pointer(self,pointer):
-        # if self.is_full():
-        #     if self.start_pointer == self.end_pointer:
-        #         self.end_pointer +=1
-        # end on write checks
-        #if end == start report end of buffer
-        #if
         if pointer == 'start':
-            self.counter += 1
-            self.start_pointer = self.counter%self.max_size
-            # if self.start_pointer == self.max_size:
-            #     self.start_pointer = 0
-            print("start: {}, end:{}".format(self.start_pointer, self.end_pointer))
-        # if pointer == 'end':
-        #     if self.end_pointer == self.max_size:
-        #         self.end_pointer = 0
+            self.start_pointer += 1
+        if pointer == 'end':
+            self.end_pointer += 1
+
 
     def front(self):
-        if self.is_empty() is False:
-            read = self.get_at_index(self.end_pointer)
+
+        # print("start: {}, end:{}".format(self.get_start_index(), self.get_end_pointer()))
+        if self.start_pointer != self.end_pointer:
+            read = self.get_at_index(self.get_end_pointer())
             self.move_pointer('end')
-        return read
+            return read
+        else:
+            pass
+
+    def get_start_index(self):
+        return self.start_pointer%self.max_size
+
+    def get_end_pointer(self):
+        return self.end_pointer%self.max_size
 
     def enqueue(self,item):
-        if self.is_full() is False:
-            self.ll.insert_at_index(self.start_pointer,item)
-            self.move_pointer('start')
-        else:
-            self.ll.insert_at_index(self.start_pointer,item)
-            self.move_pointer('start')
-        print(self.ll)
+        #If the buffer is not full, write to empty spaces
+        #if full overwrite buffer data
+        if self.is_empty() is True:
+            self.ll.append(item)
+        elif self.is_full() is True:
+            self.ll.replace_at_index(self.get_start_index(),item)
+        print(self.get_start_index())
+        self.move_pointer('start')
 
             # if self.start_pointer
 
 if __name__ == '__main__':
     cb = CircularBuffer()
     print(cb)
-    cb.enqueue('hi')
-    cb.enqueue('hi')
-    cb.enqueue('hi')
-    cb.enqueue('hi')
-    cb.enqueue('bye')
+    cb.enqueue('one')
+    cb.enqueue('two')
+    cb.enqueue('three')
+    cb.enqueue('four')
+    cb.enqueue('five')
+    cb.enqueue('six')
     print(cb)
     print(cb.size())
